dbdcc3055ca3cee81de05a47689c842f56110627
Sonlanggtu <sonlanggtu@gmail.com>
2019-10-31 20:04:35 +0700

    Lession10: Design Database for Web

---
+++ TeduCoreApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TeduCoreApp.Data;
+
+[assembly: HostingStartup(typeof(TeduCoreApp.Areas.Identity.IdentityHostingStartup))]
+namespace TeduCoreApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
